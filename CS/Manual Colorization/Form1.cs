80052e7666bfc0eb23054112ce6d8dd1b0041887
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/Manual Colorization/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraMap;
+using DevExpress.Utils;
+
+namespace Manual_Colorization {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        const string areaValueAttrName = "AreaValue";
+        const string polygonNameAttrName = "PolygonName";
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create a map control, set its dock style and add it to the form.
+            MapControl map = new MapControl();
+            map.Dock = DockStyle.Fill;
+            this.Controls.Add(map);
+
+            // Create a vector items layer and add it to the map.
+            VectorItemsLayer itemsLayer = new VectorItemsLayer();
+            map.Layers.Add(itemsLayer);
+
+            // Create the first triangle.
+            MapPolygon polygon1 = CreatePolygon(800, "Small triangle",
+                new GeoPoint[] { new GeoPoint(0, 0), new GeoPoint(0, 40), new GeoPoint(40, 0), new GeoPoint(0, 0) });
+            itemsLayer.Items.Add(polygon1);
+
+            // Create the second triangle.
+            MapPolygon polygon2 = CreatePolygon(1800, "Large triangle",
+                new GeoPoint[] { new GeoPoint(0, 0), new GeoPoint(0, -60), new GeoPoint(-60, 0), new GeoPoint(0, 0) });
+            itemsLayer.Items.Add(polygon2);
+
+            // Create a colorizer.
+            map.Colorizer = CreateChoroplethColorizer();
+            
+            // Specify the tooltip content.
+            map.ToolTipController = new ToolTipController() { AllowHtmlText = true };
+            itemsLayer.ToolTipPattern = "{"+polygonNameAttrName+"}=<b>{"+areaValueAttrName+"}</b>";
+
+        }
+
+
+        private MapPolygon CreatePolygon(double areaValue, string polygonName, GeoPoint[] points) {
+            MapPolygon item = new MapPolygon();
+
+            foreach (GeoPoint point in points) {
+                item.Points.Add(point);
+            }
+
+            item.Attributes.Add(new MapItemAttribute() { Name = areaValueAttrName, Type = typeof(double), Value = areaValue });
+           
+            item.Attributes.Add(new MapItemAttribute() { Name = polygonNameAttrName, Type = typeof(string), Value = polygonName });
+
+            return item;
+        }
+
+
+        private ChoroplethColorizer CreateChoroplethColorizer() {
+            // Create a choropleth colorizer.
+            ChoroplethColorizer colorizer = new ChoroplethColorizer();
+
+            // Specify the attribute that provides data values for the colorizer.
+            colorizer.ValueProvider = new ShapeAttributeValueProvider() { AttributeName = areaValueAttrName };
+
+            // Specify range stops for the colorizer.
+            colorizer.RangeStops.AddRange(new List<double> { 0, 1000, 2000 });
+
+            // Specify colors for the colorizer.
+            colorizer.Colors.AddRange(new List<Color> { Color.Yellow, Color.Red });
+
+            ColorScaleLegend legend = new ColorScaleLegend();
+            legend.Header = "Area";
+            colorizer.Legend = legend;
+
+            return colorizer;
+        }
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# How to color manually added shapes using the choropleth colorizer
+
+
+<p>This example demonstrates how to paint  two triangles on a map using the choropleth colorizer. </p><br />
+
+
+
+<h3>Description</h3>
+
+<p>To accomplish this task for map shapes, do the following:</p><p>1) Create a choropleth colorizer using the <a href="http://help.devexpress.com/#WindowsForms/DevExpressXtraMapMapControl_Colorizertopic"><u>MapControl.Colorizer</u></a> property; </p><p>2) Specify range stops (data splits in ranges) for the colorizer using the <a href="http://help.devexpress.com/#WindowsForms/DevExpressXtraMapChoroplethColorizer_RangeStopstopic"><u>ChoroplethColorizer.RangeStops</u></a> property;</p><p>3) Specify a set of colors in the <a href="http://help.devexpress.com/#WindowsForms/clsDevExpressXtraMapColorCollectiontopic"><u>ColorCollection</u></a> object, which is accessed via the <a href="http://help.devexpress.com/#WindowsForms/DevExpressXtraMapMapColorizer_Colorstopic"><u>MapColorizer.Colors</u></a> property; </p><p>4)  Create a <a href="http://help.devexpress.com/#WindowsForms/clsDevExpressXtraMapMapItemAttributetopic"><u>MapItemAttribute</u></a> object  using the <a href="http://help.devexpress.com/#WindowsForms/DevExpressXtraMapMapItem_Attributestopic"><u>MapItem.Attributes</u></a>  property;</p><p>5)  Specify the attribute name, type, and value using the corresponding properties (<a href="http://help.devexpress.com/#WindowsForms/DevExpressXtraMapMapItemAttribute_Nametopic"><u>MapItemAttribute.Name</u></a>, <a href="http://help.devexpress.com/#WindowsForms/DevExpressXtraMapMapItemAttribute_Typetopic"><u>MapItemAttribute.Type</u></a>, and <a href="http://help.devexpress.com/#WindowsForms/DevExpressXtraMapMapItemAttribute_Valuetopic"><u>MapItemAttribute.Value</u></a>);</p><p>6) Assign the <a href="http://help.devexpress.com/#WindowsForms/clsDevExpressXtraMapShapeAttributeValueProvidertopic"><u>ShapeAttributeValueProvider</u></a> object with the specified attribute name (<a href="http://help.devexpress.com/#WindowsForms/DevExpressXtraMapShapeAttributeValueProvider_AttributeNametopic"><u>ShapeAttributeValueProvider.AttributeName</u></a>) to the <a href="http://help.devexpress.com/#WindowsForms/DevExpressXtraMapChoroplethColorizer_ValueProvidertopic"><u>ChoroplethColorizer.ValueProvider</u></a> property.</p>
+
+<br/>
+
+
